b144d20730a8e596fea4bad970360dc5f2166cbf
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-03 23:42:02 +0530

    Implemnted setState function to access and modify state

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,27 +12,50 @@ class MovieCard extends React.Component {
       rating: 8.5,
       star: 0,
     };
+    this.addStar = this.addStar.bind(this); //if we want to use normal fucntion (not arrow function then we need to bind that function with this to access state)
   }
+
+  addStar() {
+    if (this.state.star == 5) {
+      return;
+    }
+    this.setState({ star: this.state.star + 1 }); //this.setState will re-render the ui when we make changes to state
+    console.log("Star added", this.state.star);
+  }
+
+  //   addStar = () => {
+  //     // this.state.star += 1; //this wont re-render the component and hence wont updater on ui
+
+  //     if (this.state.star == 5) {
+  //       return;
+  //     }
+  //     this.setState({ star: this.state.star + 1 }); //this.setState will re-render the ui when we make changes to state
+  //     console.log("Star added", this.state.star);
+  //   };
   render() {
+    const { title, desc, price, poster, rating, star } = this.state;
     return (
       <div className="main">
         <div className="left">
-          <img src="" alt="movie-image" />
+          <img src={poster} alt="movie-image" />
         </div>
         <div className="right">
-          <div className="movie-title">{this.state.title}</div>
-          <div className="movie-description">{this.state.desc}</div>
-          <div className="movie-prive">{this.state.price}</div>
+          <div className="movie-title">{title}</div>
+          <div className="movie-description">{desc}</div>
+          <div className="movie-prive">{price}</div>
 
           {/* footer */}
           <div className="footer">
-            <div>Rating:{this.state.rating}</div>
+            <div>Rating:{rating}</div>
 
             <div>
-              <span className="btn"> + </span>
+              <span className="btn" onClick={this.addStar}>
+                {" "}
+                +{" "}
+              </span>
               <span className=""> * </span>
               <span className="btn"> - </span>
-              <span>{this.state.star}</span>
+              <span>{star}</span>
             </div>
 
             <div style={{ display: "flex" }}>
